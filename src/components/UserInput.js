22b8f4183128e9c5a7f3ff0a8fdfb870681b698c
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-15 22:15:11 +0000

    automatically backed up by learn

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -4,8 +4,25 @@ class UserInput extends Component {
   render() {
     return(
       <div>
-        <button onClick={this.handleOnClick}>Click</button>
-        <p>{this.props.items.length}</p>
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+        <ul>
+        <li><input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={(event) => this.handleChange(event)}
+              placeholder="username" />
+        </li>
+        <li><input
+              type="text"
+              name="hometown"
+              value={this.state.hometown}
+              onChange={(event) => this.handleChange(event)}
+              placeholder="hometown" />
+        </li>
+        </ul>
+        <input type="submit" />
+        </form>
       </div>
     );
   }
