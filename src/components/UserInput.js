bad3e91368ebbb0ef381432e57124e7845f1a5fc
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-15 22:16:44 +0000

    automatically backed up by learn

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      hometown: ''
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value;
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: this.state
+    });
+  }
+
+  
   render() {
     return(
       <div>
